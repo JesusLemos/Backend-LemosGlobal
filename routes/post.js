17adc1c17f8f4fc8c04f7555d5f58407fd60bf24
JesusLemos <jesuslemosfoncubierta@gmail.com>
2020-07-18 10:14:36 +0200

    Añadido el controlador de actualizarPost

--- routes/post.js
+++ routes/post.js
@@ -4,9 +4,9 @@ var router = express.Router();
 //controladores
 const crearPost = require('../controller/crearPost_controller');
 const leerPost = require('../controller/leerPost_controller');
-
+const actualizarPost = require('../controller/actualizarPost_controller');
 
 router.get('/leer/:id', leerPost);
-router. post('/crear', crearPost);
-
+router.post('/crear', crearPost);
+router.patch('/actualizar/:id', actualizarPost);
 module.exports = router;
